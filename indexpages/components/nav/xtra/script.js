00d4ca9f5c24dc9d5e5748dfd3cb1d69588e799c
Madeleine <mgleahy@nic.edu>
2023-05-09 16:31:09 -0700

    rearranged

--- indexpages/components/nav/xtra/script.js
+++
@@ -1,4 +0,0 @@
-// const toggleButton = document.getElementsByClassName ('toggle-button') [0]
-// const navbarLinks = document.getElementsByClassName ('navbar-links') [0]
-
-// toggleButton.addEventListener ('click', () => { navbarLinks.classlist.toggle ('active') } )
